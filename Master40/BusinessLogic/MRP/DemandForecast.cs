92e76c2cf7bc1d6dec4099db66bbe9c011befafc
SeifertS <Sonne.Seifert@web.de>
2017-04-26 15:37:16 +0200

    example for forecast

--- Master40/BusinessLogic/MRP/DemandForecast.cs
+++ Master40/BusinessLogic/MRP/DemandForecast.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Master40.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Master40.BusinessLogic.MRP
 {
@@ -13,9 +15,15 @@ namespace Master40.BusinessLogic.MRP
 
     class DemandForecast : IDemandForecast
     {
+        private MasterDBContext _context;
+        DemandForecast(MasterDBContext context)
+        {
+           _context = context;
+        }
+
         void IDemandForecast.NetRequirement()
         {
-
+           var b =  _context.ArticleBoms.Include(a => a.Article);
         }
 
         void IDemandForecast.GrossRequirement()
