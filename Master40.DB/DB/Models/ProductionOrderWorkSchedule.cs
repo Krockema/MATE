0a994c31d535f52325a4fed26b16335fad6706a7
Krockema <martin.krockert@gmail.com>
2017-06-08 11:15:05 +0200

    Pows: add MachineGroup

--- Master40.DB/DB/Models/ProductionOrderWorkSchedule.cs
+++ Master40.DB/DB/Models/ProductionOrderWorkSchedule.cs
@@ -10,8 +10,10 @@ namespace Master40.DB.Models
         public int Duration { get; set; }
         public int? MachineToolId { get; set; }
         public MachineTool MachineTool { get; set; }
+        public int MachineGroupId { get; set; }
+        public MachineGroup MachineGroup { get; set; }
         public int MachineId { get; set; }
-        public Machine Machines { get; set; }
+        public Machine Machine { get; set; }
         public int ProductionOrderId { get; set; }
         public ProductionOrder ProductionOrder { get; set; }
         public int Start { get; set; }
