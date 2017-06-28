961dc868722d6543eb69ea2ba09e0febbe506889
Krockema <martin.krockert@gmail.com>
2017-06-28 15:17:30 +0200

    More Refactorings

--- Master40.DB/Data/Helper/LinqExtensions.cs
+++ Master40.DB/Data/Helper/LinqExtensions.cs
@@ -18,5 +18,72 @@ namespace Master40.DB.Data.Helper
                     prop.SetValue(dest, prop.GetValue(source, null), null);
             }
         }
+
+        /// <summary>
+        /// Return item and all children recursively.
+        /// </summary>
+        /// <typeparam name="T">Type of item.</typeparam>
+        /// <param name="item">The item to be traversed.</param>
+        /// <param name="childSelector">Child property selector.</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Traverse<T>(this T item, Func<T, T> childSelector)
+        {
+            var stack = new Stack<T>(new T[] {item});
+
+            while (stack.Any())
+            {
+                var next = stack.Pop();
+                if (next != null)
+                {
+                    yield return next;
+                    stack.Push(childSelector(next));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Return item and all children recursively.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item"></param>
+        /// <param name="childSelector"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Traverse<T>(this T item, Func<T, IEnumerable<T>> childSelector)
+        {
+            var stack = new Stack<T>(new T[] {item});
+
+            while (stack.Any())
+            {
+                var next = stack.Pop();
+                //if(next != null)
+                //{
+                yield return next;
+                foreach (var child in childSelector(next))
+                {
+                    stack.Push(child);
+                }
+                //}
+            }
+        }
+
+        /// <summary>
+        /// Return item and all children recursively.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <param name="childSelector"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Traverse<T>(this IEnumerable<T> items,
+            Func<T, IEnumerable<T>> childSelector)
+        {
+            var stack = new Stack<T>(items);
+            while (stack.Any())
+            {
+                var next = stack.Pop();
+                yield return next;
+                foreach (var child in childSelector(next))
+                    stack.Push(child);
+            }
+        }
     }
 }
