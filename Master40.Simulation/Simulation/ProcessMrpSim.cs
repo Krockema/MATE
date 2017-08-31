b4af03064b6e62bbe275c55ce903404158b8501f
SeifertS <Sonne.Seifert@web.de>
2017-08-31 11:09:34 +0200

    inmemory fix

--- Master40.Simulation/Simulation/ProcessMrpSim.cs
+++ Master40.Simulation/Simulation/ProcessMrpSim.cs
@@ -10,7 +10,7 @@ namespace Master40.Simulation.Simulation
 {
     public class ProcessMrpSim : ProcessMrp , IProcessMrp
     {
-        public ProcessMrpSim(ProductionDomainContext ctx, IMessageHub msgHub) : 
+        public ProcessMrpSim(MasterDBContext ctx, IMessageHub msgHub) : 
             base(ctx, new Scheduling(ctx), new CapacityScheduling(ctx), msgHub, new RebuildNets(ctx) )
         {
             // copy aditional Tables from Realcontext (needs to be Injected as well) 
