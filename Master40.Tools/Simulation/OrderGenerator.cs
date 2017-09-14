0e891af071618b31ce68368ff5720feff3293cec
Krockema <martin.krockert@gmail.com>
2017-09-14 09:22:37 +0200

    Project MathNet Addidtion. Refactor MessageSystem-> Tools

--- Master40.Tools/Simulation/OrderGenerator.cs
+++ Master40.Tools/Simulation/OrderGenerator.cs
@@ -1,4 +1,4 @@
-﻿using Master40.DB.Data.Context;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +7,6 @@ namespace Master40.Tools.Simulation
 {
     public class OrderGenerator
     {
-        private readonly ProductionDomainContext _context;
+       // private readonly ProductionDomainContext _context;
     }
 }
