e4557e0c2edbdc0f56831984105a6736db01f389
pascalschumann <18259965+pascalschumann@users.noreply.github.com>
2019-04-10 16:21:19 +0200

    minor

--- Zpp/Program.cs
+++ Zpp/Program.cs
@@ -1,30 +1,35 @@
 ﻿using System;
+using System.Linq;
+using Master40.DB.Data.Context;
+using Master40.DB.Models;
+using Master40.Tools.Simulation;
+using Master40.XUnitTest.DBContext;
+using MathNet.Numerics.Distributions;
+using Microsoft.EntityFrameworkCore;
 using NLog;
 
 namespace Zpp
 {
     class Program
     {
-        bool IS_DB_INITIALIZED = false;
+        private static Logger logger = NLog.LogManager.GetCurrentClassLogger();
         
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var logger = NLog.LogManager.GetCurrentClassLogger();
-            logger.Info("Hello World");
-                for (int i = 0; i < 10 ; i++)
-                {
-                    Console.WriteLine(i);
-                }
-
-                ;
+            
+            logger.Info("Starting ZPP.");
 
                 testMethod();
         }
 
         private static void testMethod()
         {
-            Console.WriteLine("my testMethod()!");
+            ProductionDomainContext _ctx = new ProductionDomainContext(new DbContextOptionsBuilder<MasterDBContext>()
+                .UseInMemoryDatabase(databaseName: "InMemoryDB")
+                .Options);
+            OrderGenerator.GenerateOrdersSyncron(_ctx, ContextTest.TestConfiguration(), 1, true); // .RunSynchronously();
+            logger.Info("Orders created.");
         }
+        
     }
 }
