5b378e5707680d71159ea4964c852c46051e47ea
Martin Krockert <martin.krockert@gmail.com>
2019-05-03 09:08:19 +0200

    simulation cli

--- Master40.Simulation/CLI/HelpComand.cs
+++ Master40.Simulation/CLI/HelpComand.cs
@@ -9,9 +9,9 @@ namespace Master40.Simulation.CLI
         public static void PrintHelp()
         {
             Console.Write(@"This is CLI Interface to AkkaSim" +
-                          " -h, -help, -?, /? : Display this" +
+                          " -h, -help, -?, /? : Display this!" +
                           " -simtype <Type> : Specify simulation Type <Central/Decentral>" +
-                          " -config <id> : Specify the simulationId to run with");
+                          " -config <id> : Specify the simulationId to run with.");
         }
     }
 }
