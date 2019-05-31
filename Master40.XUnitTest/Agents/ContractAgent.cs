25d86b8813de215aa668d7981056977fee312ab1
Martin Krockert <Krockema@users.noreply.github.com>
2019-05-31 14:46:15 +0200

    travis - init (#17)
    * travis - init
    
    * added stage installer.
    
    * fix yaml file
    
    * fix AssetTargetFallback
    
    * Package Fix AssetTargetFallback
    
    * Bower is depricated -> moved all *.js references to NPM
    
    * removed depricated package manager

--- Master40.XUnitTest/Agents/ContractAgent.cs
+++ Master40.XUnitTest/Agents/ContractAgent.cs
@@ -2,23 +2,19 @@
 using Akka.TestKit.Xunit;
 using Master40.DB.Data.Context;
 using Master40.DB.Data.Initializer;
+using Master40.DB.DataModel;
 using Master40.SimulationCore.Agents;
+using Master40.SimulationCore.Agents.ContractAgent;
+using Master40.SimulationCore.Agents.StorageAgent;
 using Master40.SimulationCore.Helper;
 using Master40.SimulationCore.MessageTypes;
 using Master40.SimulationImmutables;
-using Master40.Simulation.CLI;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
+using Master40.SimulationCore.Agents.Guardian;
 using Xunit;
-using AkkaSim.Definitions;
-using Master40.DB.DataModel;
-using Master40.SimulationCore.Agents.ContractAgent;
-using Master40.SimulationCore.Agents.DirectoryAgent;
-using Master40.SimulationCore.Agents.StorageAgent;
-using Master40.SimulationCore.Agents.SupervisorAegnt;
 using Master40.XUnitTest.Moc;
 
 namespace Master40.XUnitTest.Agents
@@ -36,43 +32,24 @@ namespace Master40.XUnitTest.Agents
 
         public ContractAgent()
         {
-
-
             //_ctx.Database.EnsureDeleted();
             MasterDBInitializerSmall.DbInitialize(_ctx);
             ResultDBInitializerBasic.DbInitialize(_resultContext);
-            //MasterDBInitializerSmall.DbInitialize(_ctx);
-            //_productionDomainContext.Database.EnsureDeleted();
-            //_productionDomainContext.Database.EnsureCreated();
-            //MasterDBInitializerLarge.DbInitialize(_productionDomainContext);
-
         }
 
         [Fact]
-        public void MocUpTest()
+        public void SimulationMocUp()
         {
             var contextProbe = this.CreateTestProbe("Context");
             var targetProbe = this.CreateTestProbe("Target");
             var sourceProbe = this.CreateTestProbe("Source");
 
-            var simContext = Sys.ActorOf(SimulationContextMoc.Props(contextProbe));
+            var simContext = Sys.ActorOf(Moc.SimulationContext.Props(contextProbe));
 
             sourceProbe.Send(simContext, new TestMessage(targetProbe, true));
 
             AwaitCondition(() => targetProbe.HasMessages);
             contextProbe.ExpectMsg<TestMessage>(a => (bool)a.Message == true);
-        }
-
-        [Fact]
-        public async Task PruneTest()
-        {
-            var tree = new Tree();
-            var what = tree;
-            if (what == null)
-            {
-                throw new NullReferenceException();
-            }
-
         }
 
         [Fact]
@@ -95,7 +72,7 @@ namespace Master40.XUnitTest.Agents
             });
         }
 
-
+                                                                                       
         [Fact]
         public void ContractTestCreateDispoAgent()
         {
@@ -103,91 +80,32 @@ namespace Master40.XUnitTest.Agents
             var testProbe = this.CreateTestProbe("ContextProbe");
             var systemProbe = this.CreateTestProbe("SystemProbe");
             var directoryProbe = this.CreateTestProbe("StorageDirectoryProbe");
+            var guardianProbe = this.CreateTestProbe("ContractGuardian");
             var inbox = Inbox.Create(Sys);
-            var simContext = Sys.ActorOf(SimulationContextMoc.Props(testProbe));
+            var simContext = Sys.ActorOf(Moc.SimulationContext.Props(testProbe));
             var agentPaths = new ActorPaths(simContext, inbox.Receiver);
             agentPaths.SetSystemAgent(systemProbe);
             agentPaths.SetStorageDirectory(directoryProbe);
-
+            var contractGuard = Sys.ActorOf(Guardian.Props(agentPaths, 0, true), "ContractGuard");
+                simContext.Tell(BasicInstruction.Initialize.Create(contractGuard, GuardianBehaviour.Get(CreatorOptions.ContractCreator)));
+                agentPaths.Guardians.Add(GuardianType.Dispo, contractGuard);
 
             // create Contract Agent and inject behaviour
             var contract = Sys.ActorOf(Contract.Props(actorPaths: agentPaths
-                                                               , time: 1
-                                                               , debug: true), "Contract-Test-Agent");
-            var behave = ContractBehaviour.Get();
-            simContext.Tell(BasicInstruction.Initialize.Create(contract, behave));
+                                                       , time: 1
+                                                       , debug: true), "Contract-Test-Agent");
+            simContext.Tell(BasicInstruction.Initialize.Create(contract, ContractBehaviour.Get()));
 
 
             // Create a Order with custom Article
             var order = new T_CustomerOrder() { DueTime = 0, Id = 1 };
-            var orderPart = new T_CustomerOrderPart() { Article = new M_Article() { Name = "Bear" }, Quantity = 1, Id = 1, CustomerOrderId = 1, CustomerOrder = order };
+            var orderPart = new T_CustomerOrderPart() { Article = new M_Article { Name = "Bear" }, Quantity = 1, Id = 1, CustomerOrderId = 1, CustomerOrder = order };
             // tell teh Contract agent
             simContext.Tell(Contract.Instruction.StartOrder.Create(orderPart, contract));
 
             // check if Child is Created correctly and sending its fist Request to the StorageProbe
             dynamic item = directoryProbe.ReceiveOne(TimeSpan.FromSeconds(50));
             Assert.Equal(item.Message, "Bear");
-            
-            // ExpectMsg("done", TimeSpan.FromSeconds(1));
-            //
-            // // the action needs to finish within 3 seconds
-            // Within(TimeSpan.FromSeconds(3), () => {
-            //     subject.Tell("hello", this.TestActor);
-            //
-            //     // This is a demo: would normally use expectMsgEquals().
-            //     // Wait time is bounded by 3-second deadline above.
-            //     AwaitCondition(() => probe.HasMessages);
-            //
-            //     // response must have been enqueued to us before probe
-            //     ExpectMsg("world", TimeSpan.FromSeconds(0));
-            //     // check that the probe we injected earlier got the msg
-            //     probe.ExpectMsg("hello", TimeSpan.FromSeconds(0));
-            //
-            //     Assert.Equal(TestActor, probe.Sender);
-            //
-            //     // Will wait for the rest of the 3 seconds
-            //     ExpectNoMsg();
-            // });
-
-        }
-        [Fact]
-        public async void InitialisationAsync()
-        {
-            var simContext = new SimulationCore.AgentSimulation(true, _ctx, _resultContext, new ConsoleHub());
-            var simConfig = _resultContext.SimulationConfigurations.FirstOrDefault();
-            simConfig.OrderQuantity = 0;
-            var simulation = await simContext.InitializeSimulation(simConfig, new SimulationConfig(false, 480));
-
-            // simulation.ActorSystem.EventStream.Subscribe(testProbe, typeof(DirectoryAgent.Instruction.CreateMachineAgents));
-
-            await Task.Delay(5000);
-            // 
-            var orderParts = _ctx.CustomerOrderParts.Single(x => x.Id == 1);
-
-            var contract = Supervisor.Instruction.CreateContractAgent.Create(orderParts, simContext.ActorPaths.SystemAgent.Ref);
-            simulation.SimulationContext.Tell(contract, ActorRefs.NoSender);
-
-            // simContext.Run();
-        }
-
-
-        [Fact]
-        public void DirectoryTest()
-        {
-            var simContext = this.CreateTestProbe();
-            var inbox = Inbox.Create(Sys);
-            var agentPaths = new ActorPaths(simContext, inbox.Receiver);
-            var directory = Sys.ActorOf(Directory.Props(agentPaths, 0, true), "DirectoryAgent");
-
-        }
-
-        [Fact]
-        public async Task DispoArticleRequestFromStorage()
-        {
-            var simContext = new SimulationCore.AgentSimulation(true, _ctx, _resultContext, new ConsoleHub());
-            var simConfig = _resultContext.SimulationConfigurations.First();
-            simContext.InitializeSimulation(simConfig, new SimulationConfig(false, 480)).Wait();
-            simContext.Run();
         }
 
         [Fact]
@@ -208,6 +126,35 @@ namespace Master40.XUnitTest.Agents
 
             Within(TimeSpan.FromSeconds(5), () => simContext.HasMessages);
            
+        }
+
+        [Fact]
+        public void CreateDispoAgent()
+        {
+            // init System
+            var simContext = this.CreateTestProbe();
+            var simSystem = this.CreateTestProbe();
+            var contractGuard = this.CreateTestProbe();
+            var inbox = Inbox.Create(Sys);
+            var agentPaths = new ActorPaths(simContext, inbox.Receiver);
+            agentPaths.SetSystemAgent(simSystem);
+            agentPaths.SetHubDirectoryAgent(this.TestActor);
+            agentPaths.AddGuardian(GuardianType.Contract, contractGuard);
+            // init Contract
+            var contract = Sys.ActorOf(Contract.Props(actorPaths: agentPaths
+                                                                   , time: 1
+                                                                   , debug: true), "Contract-Test-Agent");
+            simContext.Tell(BasicInstruction.Initialize.Create(contract, ContractBehaviour.Get()));
+
+
+            // Create a Order with custom Article
+            var order = new T_CustomerOrder() { DueTime = 0, Id = 1 };
+            var orderPart = new T_CustomerOrderPart() { Article = new M_Article { Name = "Bear" }, Quantity = 1, Id = 1, CustomerOrderId = 1, CustomerOrder = order };
+            // tell teh Contract agent
+            simContext.Tell(Contract.Instruction.StartOrder.Create(orderPart, contract));
+
+            Within(TimeSpan.FromSeconds(5), () => contractGuard.HasMessages);
+
         }
     }
 }
