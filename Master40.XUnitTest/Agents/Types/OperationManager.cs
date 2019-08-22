7b00b04dc839cc928c0b79ba93947a17b37be0da
Martin Krockert <martin.krockert@gmail.com>
2019-08-22 08:37:14 +0200

    Took Remote to solve Conflict

---
+++ Master40.XUnitTest/Agents/Types/OperationManager.cs
@@ -0,0 +1,66 @@
+﻿using Master40.DB.Data.Context;
+using Master40.DB.Data.Initializer;
+using Master40.DB.DataModel;
+using Master40.SimulationCore.Agents.ProductionAgent.Types;
+using Master40.XUnitTest.Preparations;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Master40.XUnitTest.Agents.Types
+{
+    public class OperationManagers
+    {
+        private ProductionDomainContext dbContext;
+        public OperationManagers()
+        {
+            dbContext = new ProductionDomainContext(options: new DbContextOptionsBuilder<MasterDBContext>()
+                .UseSqlServer(connectionString: Dbms.getDbContextString())
+                .Options);
+            dbContext.Database.EnsureCreated();
+            MasterDBInitializerSimple.DbInitialize(context: dbContext);
+        }
+
+        [Fact]
+        public void AddAndRetrieveOperationByKey()
+        {
+            var operationManager = new OperationManager();
+            var job = TypeFactory.CreateJobItem(jobName: "Sample Operation 1", jobDuration: 10);
+            operationManager.AddOperation(job);
+            var returnedJob = operationManager.GetByOperationKey(job.Key);
+
+            Assert.True(returnedJob != null);
+        }
+
+        [Fact]
+
+        public void GetOperationsBySkill()
+        {
+            var operationManager = new OperationManager();
+            var job = TypeFactory.CreateJobItem(jobName: "Sample Operation 1", jobDuration: 10);
+            var returnedJob = operationManager.GetOperationBySkill("Sewing");
+            Assert.True(returnedJob != null);
+        }
+
+        [Fact]
+        public void SetArticleProvided()
+        {
+            var operationManager = new OperationManager();
+            var job = TypeFactory.CreateJobItem(jobName: "Sample Operation 1", jobDuration: 10);
+            operationManager.AddOperation(job);
+            operationManager.CreateRequiredArticles(new M_Article { Name = "Bear" })
+            Assert.True(returnedJob != null);
+        }
+        [Fact]
+        public void UpdateOperations() { }
+        [Fact]
+        public void ProvideArticle()
+        {
+            OperationManager.
+            var job = TypeFactory.CreateJobItem(jobName: "Sample Operation 1", jobDuration: 10);
+            OperationManager.AddOperation(job);
+            var returnedJob = OperationManager.GetByOperationKey(job.Key);
+
+            Assert.True(returnedJob != null);
+        }
+    }
+}
---
+++ Master40.XUnitTest/Preparations/TypeFactory.cs
@@ -0,0 +1,25 @@
+﻿using Akka.Actor;
+using Master40.DB.DataModel;
+using Master40.SimulationCore.Helper;
+using static FOperations;
+
+namespace Master40.XUnitTest.Preparations
+{
+    public static class TypeFactory
+    {
+        public static FOperation CreateJobItem(string jobName, int jobDuration, bool preCondition = true, int dueTime = 50, string skillName = "Sewing")
+        {
+            var operation = new M_Operation()
+            {
+                ArticleId = 10,
+                AverageTransitionDuration = 20,
+                Duration = jobDuration,
+                HierarchyNumber = 10,
+                Id = 1,
+                Name = jobName,
+                ResourceSkill = new M_ResourceSkill() { Name = skillName }
+            };
+            return operation.ToOperationItem(dueTime: 50, productionAgent: ActorRefs.Nobody, firstOperation: preCondition, currentTime: 0);
+        }
+    }
+}
