666a65188cd486e587ec736a05b0f2dbde7bf5be
Marvin Matthes <marvin.matthes@htw-dresden.de>
2020-08-25 15:06:13 +0200

    intial push central planing

---
+++ Master40.SimulationCore/Agents/HubAgent/Behaviour/Central.cs
@@ -0,0 +1,34 @@
+﻿using Master40.DB.Nominal;
+using Master40.SimulationCore.Helper.DistributionProvider;
+using System;
+using static FResourceInformations;
+
+
+namespace Master40.SimulationCore.Agents.HubAgent.Behaviour
+{
+    class Central : SimulationCore.Types.Behaviour
+    {
+
+        public Central(long maxBucketSize, WorkTimeGenerator workTimeGenerator, SimulationType simulationType = SimulationType.Default) : base(childMaker: null, simulationType: simulationType)
+        {
+        }
+
+            public override bool Action(object message)
+            {
+                var success = true;
+                switch (message)
+                {
+                    //Initialize
+                    case Hub.Instruction.Default.AddResourceToHub msg: CreateOperations(resourceInformation: msg.GetObjectFromMessage); break;
+
+                    default: return false;
+                }
+                return success;
+            }
+
+        private void CreateOperations(FResourceInformation resourceInformation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
