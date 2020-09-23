cdb4754ed71c6595f71d6f6deb0f53a1e88a5d85
Krockema <martin.krockert@htw-dersden.de>
2020-09-23 15:41:39 +0200

    Simulation Results with Parent Child Hirarchie and Example Querry for SQL CTE

---
+++ Master40.XUnitTest/DataGenerator/SimulationTemplate.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using Master40.DB.Data.Context;
+using Master40.Simulation.CLI;
+using Master40.SimulationCore;
+using Master40.SimulationCore.Environment.Options;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Master40.XUnitTest.DataGenerator
+{
+    public class SimulationTemplate
+    {
+        // local TEST Context
+        private const string testCtxString = "Server=(localdb)\\mssqllocaldb;Database=TestContext;Trusted_Connection=True;MultipleActiveResultSets=true";
+        private const string testResultCtxString = "Server=(localdb)\\mssqllocaldb;Database=TestResultContext;Trusted_Connection=True;MultipleActiveResultSets=true";
+        private static int _simNr = 1;
+
+        // Definition for Simulation runs each Call returns
+        // TODO: return complete config objects to avoid errors, and separate Data Generator / Simulation configurations
+        public static IEnumerable<object[]> GetTestData()
+        {
+            // Simulation run 1
+            yield return new object[]  
+            {
+                _simNr++// simulation number
+                , 5     // order Quantity
+                , 240   // max bucket size
+                , 1920  // throughput time
+                , 1337  // Random seed
+                , 0.03  // arrival rate
+                , 2880  // simulation end
+            };
+            // Simulation run 2
+            yield return new object[]
+            {
+                _simNr++// simulation number
+                , 5     // order Quantity
+                , 240   // max bucket size
+                , 1920  // throughput time
+                , 1337  // Random seed
+                , 0.02  // arrival rate
+                , 2880  // simulation end
+            };
+        }
+
+        /// <summary>
+        /// To Run this test the Database must have been filled with Master data
+        /// </summary>
+        /// <param name="simNr"></param>
+        /// <param name="maxBucketSize"></param>
+        /// <param name="throughput"></param>
+        /// <param name="seed"></param>
+        /// <param name="arrivalRate"></param>
+        /// /// <param name="simulationEnd"></param>
+        /// <returns></returns>
+        [Theory]
+        //[InlineData(SimulationType.DefaultSetup, 1, Int32.MaxValue, 1920, 169, ModelSize.Small, ModelSize.Small)]
+        [MemberData(nameof(GetTestData))]
+        public async Task SystemTestAsync(int simNr
+                                        , int orderQuantity
+                                        , int maxBucketSize
+                                        , long throughput
+                                        , int seed
+                                        , double arrivalRate
+                                        , long simulationEnd)
+        {
+            ResultContext ctxResult = ResultContext.GetContext(resultCon: testResultCtxString);
+            ProductionDomainContext masterCtx = ProductionDomainContext.GetContext(testCtxString);
+
+            var simContext = new AgentSimulation(DBContext: masterCtx, messageHub: new ConsoleHub());
+            var simConfig = ArgumentConverter.ConfigurationConverter(ctxResult, 1);
+            
+            // update customized Configuration
+            simConfig.AddOption(new DBConnectionString(testResultCtxString));  
+            simConfig.ReplaceOption(new TimeConstraintQueueLength(480));
+            simConfig.ReplaceOption(new OrderArrivalRate(value: arrivalRate));
+            simConfig.ReplaceOption(new OrderQuantity(value: orderQuantity)); 
+            simConfig.ReplaceOption(new EstimatedThroughPut(value: throughput));
+            simConfig.ReplaceOption(new TimePeriodForThroughputCalculation(value: 2880));
+            simConfig.ReplaceOption(new Seed(value: seed));
+            simConfig.ReplaceOption(new SettlingStart(value: 0)); 
+            simConfig.ReplaceOption(new SimulationEnd(value: simulationEnd));
+            simConfig.ReplaceOption(new SaveToDB(value: true));
+            simConfig.ReplaceOption(new MaxBucketSize(value: maxBucketSize));
+            simConfig.ReplaceOption(new SimulationNumber(value: simNr));
+            simConfig.ReplaceOption(new WorkTimeDeviation(0.0));
+
+            var simulation = await simContext.InitializeSimulation(configuration: simConfig);
+
+            
+            if (simulation.IsReady())
+            {
+                // Start simulation
+                var sim = simulation.RunAsync();
+                simContext.StateManager.ContinueExecution(simulation);
+                await sim;
+                Assert.True(condition: sim.IsCompleted);
+            }
+        }
+
+        /* CTE on MasterResultContext (SQL Querry)
+         
+                DECLARE @simNr BIGINT;
+                SET @simNr = 0;
+
+                WITH DirectReports(ProductionOrderId, JobName, Parent,CapabilityProvider, CapabilityName, HirachyNumber, BomLevel) 
+                AS
+                (       SELECT jobs.ProductionOrderId, jobs.JobName, jobs.ParentId, jobs.CapabilityProvider, jobs.CapabilityName, HierarchyNumber, 0 AS BomLevel
+                        FROM SimulationJobs as jobs
+		                WHERE jobs.ProductionOrderId = 'ProductionAgent(0)' and jobs.SimulationNumber = @simNr
+                        
+	                UNION ALL
+                        
+		                SELECT e.ProductionOrderId, e.JobName, e.Parent, e.CapabilityProvider, e.CapabilityName, e.HierarchyNumber, BomLevel + 1
+                        FROM SimulationJobs as e
+                        INNER JOIN DirectReports AS dr ON dr.ProductionOrderId = e.Parent and e.SimulationNumber =  @simNr
+                )
+                    SELECT distinct *
+                    FROM DirectReports
+	                order by BomLevel, ProductionOrderId ,HirachyNumber
+
+         */
+
+    }
+}
