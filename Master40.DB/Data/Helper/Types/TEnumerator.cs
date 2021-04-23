dd570b261533b462ed639aa31280eca2745435eb
Krockema <martin.krockert@htw-dersden.de>
2021-04-23 09:29:38 +0200

    More Cleanup

--- Master40.DB/Data/Helper/Types/TEnumerator.cs
+++ Master40.DB/Data/Helper/Types/TEnumerator.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using System.Transactions;
-using Master40.DB.DataModel;
 
 namespace Master40.DB.Data.Helper.Types
 {
