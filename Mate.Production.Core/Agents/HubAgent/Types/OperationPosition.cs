1c9aefb6c00ed9406df07edb297beb3a96552e7c
Marvin <marvin.matthes@googlemail.com>
2022-05-12 08:52:17 +0200

    on progress

---
+++ Mate.Production.Core/Agents/HubAgent/Types/OperationPosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mate.Production.Core.Agents.HubAgent.Types
+{
+    internal class OperationPosition : Tuple<int, string>
+    {
+        public OperationPosition(int item1, string item2) : base(item1, item2)
+        {
+
+        }
+    }
+}
